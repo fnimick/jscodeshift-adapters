cd02fef496e50bce8dc6677da1a60ed5fa62c827
Francis Nimick <fnimick@gmail.com>
2024-07-25 16:41:32 -0400

    Make types more flexible

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,15 +1,17 @@
-import { Transform } from "jscodeshift";
+import { API, FileInfo, Options, Transform } from "jscodeshift";
 
 /**
  * This is necessary as @types/jscodeshift does not account for returning a Promise from a
  * transform function, which is supported by jscodeshift.
  * See: https://github.com/DefinitelyTyped/DefinitelyTyped/discussions/70137
  */
-export type TransformWithPromise = (
-	...args: Parameters<Transform>
+export type TransformWithPromise<TransformOptions extends Options = Options> = (
+	file: FileInfo,
+	api: API,
+	options: TransformOptions,
 ) => ReturnType<Transform> | Promise<ReturnType<Transform>>;
 
-export type Adapter = (
-	transform: TransformWithPromise,
-	...args: Parameters<Transform>
-) => ReturnType<TransformWithPromise>;
+export type Adapter<TransformOptions extends Options = Options> = (
+	transform: TransformWithPromise<TransformOptions>,
+	...args: Parameters<TransformWithPromise<TransformOptions>>
+) => ReturnType<TransformWithPromise<TransformOptions>>;
